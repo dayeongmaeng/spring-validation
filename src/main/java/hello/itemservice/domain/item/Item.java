814cf72662c4bf56d2c00addb653532860bfe3ca
Dayeong <dayeongmaeng@gmail.com>
2023-04-19 22:48:41 +0900

    feat: Form 전송 객체 분리 - 개발

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,10 +1,6 @@
 package hello.itemservice.domain.item;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 @Data
 /*@ScriptAssert( //이 검증까지 가는 건 무리. 제약이 많고 복잡함, 오브젝트 오류는 자바 코드로 처리하는 것을 권장
@@ -13,16 +9,16 @@ import org.hibernate.validator.constraints.Range;
     message = "총합이 10,000원 넘게 입력해주세요.")*/
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+    //    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    //    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    //    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항 추가
+    //    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
