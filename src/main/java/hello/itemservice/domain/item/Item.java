e14430fa7dc19fd6eaf51ac522bc3cc2948717b0
Dayeong <dayeongmaeng@gmail.com>
2023-04-19 21:57:39 +0900

    feat: Bean Validation - 에러 코드

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,7 +10,7 @@ import org.hibernate.validator.constraints.Range;
 public class Item {
 
     private Long id;
-    @NotBlank
+    @NotBlank(message = "공백X")
     private String itemName;
     @NotNull
     @Range(min = 1000, max = 1000000)
